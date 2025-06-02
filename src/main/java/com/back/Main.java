4008a77a44a1aa2705c30fc0199839dacf7e511d
overimm <sanglim7698@gmail.com>
2025-06-02 18:06:45 +0900

    switch 및 method 이용하여 리팩토링

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -5,91 +5,122 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("== 명언 ==");
-        Scanner scanner = new Scanner(System.in);
-        int id = 0;
+    static Scanner scanner = new Scanner(System.in);
+    static List<WiseSayings> wiseSayingsList = new ArrayList<>();
+    static int id = 0;
 
-        //클래스 리스트 생성
-        List<WiseSayings> wiseSayingsList = new ArrayList<>();
+    public static void main(String[] args) {
+
+        System.out.println("== 명언 ==");
+
 
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine().trim();
 
-            if (cmd.equals("종료")) {
+            String baseCmd = cmd.contains("?") ? (cmd.split("\\?")[0]) : (cmd);
+
+            switch (baseCmd) {
+                case "종료":
+                    return;
+                case "등록":
+                    handleRegister();
+                    break;
+                case "목록":
+                    handleList();
+                    break;
+                case "삭제":
+                    handlesDelete(cmd);
+                    break;
+                case "수정":
+                    handleModify(cmd);
+                    break;
+            }
+
+        }
+    }
+
+
+    //등록 메소드
+    public static void handleRegister() {
+        System.out.print("명언 : ");
+        String wiseSay = scanner.nextLine().trim();
+
+        System.out.print("작가 : ");
+        String author = scanner.nextLine().trim();
+        id++;
+
+        //입력받은 명언 리스트에 저장
+        wiseSayingsList.add(new WiseSayings(id, wiseSay, author));
+
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+
+    }
+
+    //목록 메소드
+    public static void handleList() {
+        System.out.println("번호 / 작가 / 명언");
+
+        //리스트에 저장된 명언들 다꺼내서 출력
+        for (int i = 0; i <= wiseSayingsList.size() - 1; i++) {
+            // wiseSayings 클래스 변수 하나 만들고 거기다가 리스트에 있는 것들 계속 집어넣고 출력
+            WiseSayings w1 = wiseSayingsList.get(i);
+            System.out.printf("%d / %s / %s \n", w1.id, w1.wiseSay, w1.author);
+        }
+
+    }
+
+    //삭제 메소드
+    public static void handlesDelete(String cmd) {
+        int idToDelete = Integer.parseInt(cmd.substring("삭제?id=".length()));
+
+        boolean found = false;
+        // 입력받은 삭제id와 리스트에 아이디를 비교해야함 -> 비교하기위한 리스트 모든 값 호출
+        for (int i = 0; i < wiseSayingsList.size(); i++) {
+
+            if (wiseSayingsList.get(i).id == idToDelete) {
+                wiseSayingsList.remove(i);
+                found = true;
+                System.out.printf("%d번 명언이 삭제되었습니다.\n", idToDelete);
                 break;
-            } else if (cmd.equals("등록")) {
+            }
+        }
+        if (found == false) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", idToDelete);
+
+        }
+
+
+    }
+
+    //수정 메소드
+    public static void handleModify(String cmd) {
+        int idTomod = Integer.parseInt(cmd.substring(6));
+        boolean foundMod = false;
+
+        for (int i = 0; i < wiseSayingsList.size(); i++) {
+            if (idTomod == wiseSayingsList.get(i).id) {
+                System.out.printf("명언(기존) : %s\n", wiseSayingsList.get(i).wiseSay);
                 System.out.print("명언 : ");
-                String wiseSay = scanner.nextLine().trim();
-
+                String modifyWise = scanner.nextLine().trim();
+                System.out.printf("작가(기존) : %s\n", wiseSayingsList.get(i).author);
                 System.out.print("작가 : ");
-                String author = scanner.nextLine().trim();
-                id++;
+                String modifyAuthor = scanner.nextLine().trim();
 
-                //입력받은 명언 리스트에 저장
-                wiseSayingsList.add(new WiseSayings(id, wiseSay, author));
+                wiseSayingsList.get(i).wiseSay = modifyWise;
+                wiseSayingsList.get(i).author = modifyAuthor;
+                System.out.println("수정 완료");
 
-
-                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
-            } else if (cmd.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언");
-
-                //리스트에 저장된 명언들 다꺼내서 출력
-                for (int i = 0; i <= wiseSayingsList.size() - 1; i++) {
-                    // wiseSayings 클래스 변수 하나 만들고 거기다가 리스트에 있는 것들 계속 집어넣고 출력
-                    WiseSayings w1 = wiseSayingsList.get(i);
-                    System.out.printf("%d / %s / %s \n", w1.id, w1.wiseSay, w1.author);
-                }
-            } else if (cmd.startsWith("삭제?id=")) {
-                //"삭제?id="의 다음 입력을 받아서 int로 변환.
-                int idToDelete = Integer.parseInt(cmd.substring("삭제?id=".length()));
-
-                boolean found = false;
-                // 입력받은 삭제id와 리스트에 아이디를 비교해야함 -> 비교하기위한 리스트 모든 값 호출
-                for (int i = 0; i < wiseSayingsList.size(); i++) {
-                    int delNum = wiseSayingsList.get(i).id;
-                    if (delNum == idToDelete) {
-                        wiseSayingsList.remove(idToDelete - 1);
-                        found = true;
-                        System.out.printf("%d번 명언이 삭제되었습니다.\n", idToDelete);
-                        break;
-                    }
-                }
-                if (found == false) {
-                    System.out.printf("%d번 명언은 존재하지 않습니다.\n", idToDelete);
-                }
-            } else if (cmd.startsWith("수정?id=")) {
-                //수정받을 id 받아서 int로 변환
-                int idToModify = Integer.parseInt(cmd.substring("수정?id=".length()));
-
-                boolean found = false;
-
-                //리스트 호출하여 id랑 비교
-                for (int i = 0; i < wiseSayingsList.size(); i++) {
-                    int modifyNum = wiseSayingsList.get(i).id;
-                    if (modifyNum == idToModify) {
-                        System.out.printf("명언(기존) : %s\n", wiseSayingsList.get(idToModify - 1).wiseSay);
-                        System.out.print("명언 : ");
-                        String modifyWiseSay = scanner.nextLine().trim();
-
-                        System.out.printf("작가(기존) : %s\n", wiseSayingsList.get(idToModify - 1).author);
-                        System.out.print("명언 : ");
-                        String modifyAuthor = scanner.nextLine().trim();
-
-                        wiseSayingsList.remove(idToModify - 1);
-                        wiseSayingsList.add(new WiseSayings(idToModify, modifyWiseSay, modifyAuthor));
-                        found = true;
-                        System.out.println("수정 완료");
-                        break;
-                    }
-                }
-                //입력받은 id가 없을경우
-                if (found == false) {
-                    System.out.printf("%d번 명언은 존재하지 않습니다.\n", idToModify);
-                }
+                foundMod = true;
+                break;
 
             }
         }
+        if (foundMod == false) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", idTomod);
+        }
+
     }
+
 }
